4c2181b56ba2e5dda62125ac1479c2573177f6c3
shawon2911 <hosenuzzaman2018@gmail.com>
2026-06-29 04:22:20 +0600

    donor homepage ui done

---
+++ src/Components/Dashboard/Donor/HomePagetable.jsx
@@ -0,0 +1,145 @@
+import { Button, Chip, Table } from "@heroui/react";
+import Link from "next/link";
+
+
+const HomePagetable = ({ threeData }) => {
+  return (
+    <div>
+      <Table className="bg-paper">
+        <Table.ResizableContainer>
+          <Table.Content
+            aria-label="Recent donation requests"
+            className="min-w-[700px]"
+          >
+            <Table.Header className="bg-[#e6d8d2]">
+              <Table.Column
+                isRowHeader
+                defaultWidth="1fr"
+                id="name"
+                minWidth={160}
+                className="uppercase"
+              >
+                Recipient
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="location"
+                minWidth={220}
+                className="uppercase"
+              >
+                Location
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="date"
+                minWidth={200}
+                className="uppercase"
+              >
+                Date / Time
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="group"
+                minWidth={100}
+                className="uppercase"
+              >
+                Group
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="status"
+                minWidth={120}
+                className="uppercase"
+              >
+                Status
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="donor"
+                minWidth={120}
+                className="uppercase"
+              >
+                Donor
+                <Table.ColumnResizer />
+              </Table.Column>
+              <Table.Column
+                defaultWidth="1fr"
+                id="actions"
+                minWidth={180}
+                className="uppercase"
+              >
+                Actions
+                <Table.ColumnResizer />
+              </Table.Column>
+            </Table.Header>
+
+            <Table.Body>
+              {threeData.map((req) => (
+                <Table.Row key={req._id}>
+                  <Table.Cell>{req.recipientName}</Table.Cell>
+                  <Table.Cell>{req.fullAddress}</Table.Cell>
+                  <Table.Cell>
+                    {req.donationDate} / {req.donationTime}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <span className="bg-red-50 text-crimson font-bold px-2 py-1 rounded-lg text-xs border border-red-100">
+                      {req.bloodGroup}
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Chip
+                      size="sm"
+                      variant="soft"
+                      color={
+                        req.status === "pending"
+                          ? "warning"
+                          : req.status === "inprogress"
+                            ? "primary"
+                            : req.status === "done"
+                              ? "success"
+                              : "danger"
+                      }
+                    >
+                      {req.status}
+                    </Chip>
+                  </Table.Cell>
+                  <Table.Cell>{req.donorName || "—"}</Table.Cell>
+                  <Table.Cell>
+                    <div className="flex items-center gap-2">
+                       <Button variant="none" className={'border rounded-xl'}>
+                        <Link
+                        href={`/donation-requests/${req._id}`}
+                        className="text-xs text-crimson hover:underline"
+                      >
+                        View
+                      </Link>
+                       </Button>
+                      {
+                        req.status === "pending" ?
+                            <div className="flex items-center gap-2">
+                                <Button variant="none" className={'border rounded-xl'}>Edit</Button>
+                                <Button variant="none" className={'border border-crimson text-crimson rounded-xl'}>Delete</Button>
+                            </div> : req.status === "inprogress" ?
+                                <div className="flex items-center gap-2">
+                                    <Button variant="none" className={'border border-green-600 text-green-600 rounded-xl'}>Done</Button>
+                                    <Button variant="none" className={'border  border-crimson text-crimson rounded-xl'}>Cancel</Button>
+                                </div> : <></>
+                      }
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Content>
+        </Table.ResizableContainer>
+      </Table>
+    </div>
+  );
+};
+
+export default HomePagetable;
--- src/app/dashboard/donor/page.jsx
+++ src/app/dashboard/donor/page.jsx
@@ -1,19 +1,56 @@
-"use client"
+"use client";
+import HomePagetable from "@/Components/Dashboard/Donor/HomePagetable";
 import WelcomeMessage from "@/Components/Dashboard/WelcomeMessage";
 import { authClient } from "@/lib/auth-client";
-
+import { Chip, Table } from "@heroui/react";
+import { useEffect, useState } from "react";
 
 const DonorDashboardHomePage = () => {
-    const { data: session } = authClient.useSession();
-      const user = session?.user;
-    
-    return (
-        <div>
-            {/* welcome message */}
-            <WelcomeMessage userName={user?.name} />
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+  const email = user?.email;
 
-        </div>
-    );
+  const [donations, setDonations] = useState(null);
+
+useEffect(() => {
+  if (!email) return;
+
+  const fetchDonations = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/bloodReq?email=${email}`,
+        {
+          cache: "no-store",
+        }
+      );
+
+      const data = await res.json();
+      setDonations(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchDonations();
+}, [email]);
+
+const allData = donations?.data || [];
+// console.log(allData)
+const threeData = allData.slice(0,3);
+console.log(threeData)
+
+// console.log(allData);
+
+  return (
+    <div>
+      {/* welcome message */}
+      <WelcomeMessage userName={user?.name} />
+      <div className="md:mx-5"> 
+        <h2 className="text-ink text-xl font-bold md:ml-3 mb-2">Recent Donation Requests</h2>
+        <HomePagetable threeData={threeData} />
+      </div>
+    </div>
+  );
 };
 
 export default DonorDashboardHomePage;
